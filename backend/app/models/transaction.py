12ce4bbab2d3729586ce1f05a023110f6d60fb8c
umar <amar.m.sheikh@gmail.com>
2025-06-02 16:40:17 +0500

    Allow blank true for address in transaction model

--- backend/app/models/transaction.py
+++ backend/app/models/transaction.py
@@ -6,7 +6,7 @@ from .address import Address
 
 class Transaction(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='transactions')
-    address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True, related_name='transactions')
+    address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True, blank=True, related_name='transactions')
 
     idempotency_key = models.UUIDField(default=uuid1, editable=False, unique=True)
     amount = models.DecimalField(max_digits=5, decimal_places=2, validators=[MinValueValidator(0)])
